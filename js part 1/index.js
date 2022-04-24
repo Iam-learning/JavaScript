33f000b682f9382a84915d5454ff2396d81f18c7
HeySkidee <harshitojha46@gmail.com>
2022-04-24 23:30:50 +0530

    js tutorial (harry)

---
+++ js part 1/index.js
@@ -0,0 +1,50 @@
+    //NUMBERS
+    console.log ("Numbers: ");
+    var num1 = 23;
+    var num2 = 23.5;
+    console.log(" num1 = ",num1,'\n',"and num2 = ",num2,);
+    
+    //STRINGS
+    console.log ("Strings: ");
+    var str = "This is a string.";
+    console.log(" str = ",str);
+
+    //OBJECTS
+    console.log("Objects:");
+    var Marks = {
+        ravi: 23,
+        john: 86,
+        peter: 77.4
+    }
+    console.log(" Marks = ",Marks);
+
+    //BOOLEANS
+    console.log("Booleans:");
+    var big = true;
+    var small = false;
+    console.log(" big   = ",big,"\n small = ",small);
+
+    //UNDEFINED
+    console.log("Undefined: ");
+    var und = undefined;
+    console.log(" und = ",und);
+
+    //NULL
+    console.log("Null");
+    var n = null;
+    console.log(" n = ",n);
+
+    /* 
+    At a very high level, there are 2 types of data types in javascript
+    
+    Primitive: Numbers, Strings, Booleans, Null, Undefined, Symbol.
+    Reference: Arrays, Objects.
+
+    */
+
+    //ARRAYS
+    console.log("Arrays: ");
+    var arr = [1,2,"brr",4,5];
+    console.log(" arr = ",arr);
+    console.log(" arr[3] = ",arr[3]);
+    console.log(" arr[2] = ",arr[2]);
---
+++ js part 2/index.js
@@ -0,0 +1,77 @@
+//ASSIGNMENT OPERATORS:
+console.log("Assignments Operators: ");
+var a = 100;
+var b = 10;
+console.log(" a + b = ", a + b);
+console.log(" a - b = ", a - b);
+console.log(" a * b = ", a * b);
+console.log(" a / b = ", a / b);
+
+//LOGICAL OPERATORS:
+
+//AND OPERATOR:
+console.log("Logical AND: ");
+console.log(" t && t = ", true&&true);
+console.log(" f && f = ", false&&false);
+console.log(" t && f = ", true&&false);
+console.log(" f && t = ", false&&true);
+
+//OR OPERATOR:
+console.log("Logical OR: ");
+console.log(" t || t = ", true||true);
+console.log(" f || f = ", false||false);
+console.log(" t || f = ", true||false);
+console.log(" f || t = ", false||true);
+
+//NOT OPERATOR:
+console.log("Logical NOT: ");
+console.log(" !true = ", !true);
+console.log(" !false = ", !false);
+
+//FUNCTIONS: DRY (Do Not Repeat Yourself) concept
+console.log("Fuctions: ");
+
+function avg(a,b){
+    c = (a+b)/2;
+    return c;
+   // It can also written as:
+   // return (a+b)/2;
+}
+
+c1 = avg(54,23);
+c2 = avg(38,12);
+console.log(" avg(54,23) = ",c1);
+console.log(" avg(38,12) = ",c2);
+
+//CONDITIONAL STATEMENTS:
+console.log("Conditional Statements: ");
+
+var age = 18;
+if ( age > 18){
+    console.log(" age > 18 = ",age>18);
+    console.log(" Eligible to Vote")
+}
+else{
+    console.log(" age > 18 = ",age>18);
+    console.log(" Not Eligible to Vote")
+}
+
+//FOR LOOP
+console.log("For Loop: ");
+
+var arr = [1,2,3,4];
+console.log(" arr = ",arr);
+console.log(" Iterating an array using for loop: ")
+for (var i=0; i<arr.length; i++){
+        console.log(" ",arr[i]);
+}
+
+//alternate way:
+console.log(" doing the same thing using forEach: ")
+arr.forEach(function(element){
+    console.log(" ",element);
+})
+
+//WHILE LOOP AND USE OF LET
+console.log("Using \033[3mlet\033[0m to declare variables:");
+console.log("It has block level scope, var is used to declare global variables.")
