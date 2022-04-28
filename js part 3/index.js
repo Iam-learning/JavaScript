c003066228da0627e0bd4f159d4eb22677e93ac0
HeySkidee <harshitojha46@gmail.com>
2022-04-28 22:49:36 +0530

    dom manipulation

--- js part 3/index.js
+++ js part 3/index.js
@@ -8,7 +8,7 @@ console.log(elemClass);
 elemClass[0].style.background = 'pink';
 elemClass[1].classList.add("bg-primary");
 elemClass[2].classList.add("text-success");
-elemClass[2].classList.remove("text-success");
+// elemClass[2].classList.remove("text-success");
 
 
     
